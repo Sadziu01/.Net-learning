27e336be7d44ebee3f5e1d726ebe6b89ffb37259
Sadziu <sadziu01@gmail.com>
2023-11-24 23:28:31 +0100

    small API

---
+++ FirstAPI/FirstAPI/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SADZIUSQL;Database=superherodb;User Id=test;Password=123;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+
+}
